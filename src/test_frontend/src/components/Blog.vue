e7f380d44f74d2b1ad4dbf9d8143a7162f62e1ac
Nealz1 <nealz1337@gmail.com>
2024-06-25 19:54:54 +0200

    added displaying wpisy

--- src/test_frontend/src/components/Blog.vue
+++ src/test_frontend/src/components/Blog.vue
@@ -1,28 +1,31 @@
 <template>
-
     <div>
-        <button @click="pobierzWpisy">Refresh</button>
-        siema blog
-
+        <button @click="pobierzWpisy">refresh</button>
+        siema blog!
         {{ wpisy }}
+        <input v-model="nowyBlog" type="text">
+        <button @click="dodajWpisy">dodaj</button>
     </div>
-
 </template>
 
 <script>
 import { test_backend } from 'declarations/test_backend/index';
 
-    export default{
-        data(){
-            return{
-                wpisy: []
-            }
-        },
-        methods: {
-            async pobierzWpisy(){
-            this.wpisy =  await test_backend.odczytaj_wpisy();
-            },
+export default {
+    data() {
+        return {
+            wpisy: [],
+            nowyBlog: ""
         }
-    }
+    },
+    methods: {
+        async dodajWpisy() {
+            await test_backend.dodaj_wpis(this.nowyBlog);
+        },
+        async pobierzWpisy() {
+            this.wpisy = await test_backend.odczytaj_wpisy();
+        }
+    },
 
+}
 </script>
