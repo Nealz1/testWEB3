86320cafcb394ca2b98c787d769b9015b6916f4f
Nealz1 <nealz1337@gmail.com>
2024-06-25 19:42:22 +0200

    working blog

--- src/test_frontend/src/components/Blog.vue
+++ src/test_frontend/src/components/Blog.vue
@@ -1,23 +1,28 @@
 <template>
+
     <div>
-        Siema blog!
+        <button @click="pobierzWpisy">Refresh</button>
+        siema blog
+
         {{ wpisy }}
     </div>
+
 </template>
 
 <script>
 import { test_backend } from 'declarations/test_backend/index';
 
-export default{
-    data(){
-        return{
-            wpisy: []
+    export default{
+        data(){
+            return{
+                wpisy: []
+            }
+        },
+        methods: {
+            async pobierzWpisy(){
+            this.wpisy =  await test_backend.odczytaj_wpisy();
+            },
         }
     }
-    methods: {
-        .async pobierzWpisy(){
-            test_backend
-        }
-    },
-}
+
 </script>
